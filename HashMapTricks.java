c9516f4aa9a8f2b928423ee0778adf0a42c9e0b5
Ajit Puthenputhussery <avp38@njit.edu>
2017-07-05 12:47:57 -0400

    Update HashMapTricks.java

--- HashMapTricks.java
+++ HashMapTricks.java
@@ -1,1 +1,9 @@
+// HashMap Tricks
+
+// 1. Access all keys of HashMap & get their values
+HashMap<Character, Integer> map = new HashMap<>();
+for (char key : map.keySet()) {
+  int frequency = map.get(key); // Get value
+  ...
+}
 
