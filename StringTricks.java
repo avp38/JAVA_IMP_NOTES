9e025988f3ef7969b600ff1a5f2ce8af5ce1f5a6
Ajit Puthenputhussery <avp38@njit.edu>
2017-07-12 03:47:36 -0400

    Create StringTricks.java

---
+++ StringTricks.java
@@ -0,0 +1,10 @@
+// For char -- Important functions
+(java.lang packages)
+Character.isLetterOrDigit(char ch)
+Character.toLowerCase(char ch)
+
+// To calculate ASCII value
+char c = 'a';
+int ascii = (int) c;
+// Important ASCII VALUES
+// 65-90 --> 'A'-'Z' ||| 97-122 --> 'a'-'z' ||| 48-57 --> '0'-'9'
