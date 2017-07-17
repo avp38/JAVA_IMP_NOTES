44f23b4e35616af8148cac5be70252af67e0c062
Ajit Puthenputhussery <avp38@njit.edu>
2017-07-17 14:30:27 -0400

    Create Integer_Long_Tricks.java

---
+++ Integer_Long_Tricks.java
@@ -0,0 +1,13 @@
+// To convert long to Integer
+long a = 10;
+int b = (int)a;
+
+// Arrays .sort method --> Check all Constructors
+// Also works for character array
+char[] a ={'1','4','6','3','5'} ;
+Arrays.sort(a);
+Arrays.sort(a, 2, a.length); // To sort from index position 2 till end (Note: end is last_index+1)
+
+// To get MAX Integer val
+int maxInt = Integer.MAX_VALUE;
+// Similarly can use for min value --> Also works for long 
