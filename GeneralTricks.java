f3ec88ee81d772f723c27aa3b7808285904d8320
Ajit Puthenputhussery <avp38@njit.edu>
2017-08-09 02:36:38 -0400

    Create GeneralTricks.java

---
+++ GeneralTricks.java
@@ -0,0 +1,10 @@
+// To sort any Collections --> (ex: ArrayList,..)
+//Suppose ArrayList a;
+Collections.sort(a);
+
+
+// If you neeed powers of 2 --> ALWAYS USE Binary Manipulation Instead of Math.pow
+// Binary Manipulation is very FAST compared to Math.pow
+// NOTE: For POWERS OF TWO ONLY
+// 2^n = 1<<n; 
+// But be CAREFUL that n>=0, If nbecomes negative --> will give weird results
