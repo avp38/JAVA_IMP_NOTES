261d69aef36aea321bd19e9a93c7880974df6015
Ajit Puthenputhussery <avp38@njit.edu>
2017-08-20 19:35:43 -0400

    Update SortTricks.java

--- SortTricks.java
+++ SortTricks.java
@@ -7,3 +7,8 @@ Arrays.sort(nums)
 Arrays.sort(points, (a,b)->a[0]-b[0]);
 // NOTE: a[0]-b[0] --> Ascending Order
 // For descending order --> b[0]-a[0]
+
+
+// To sort Collections
+// Suppose we have List<Integer> list --> ArrayList
+Collections.sort(list);
