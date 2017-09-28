34aaaaac8aee3cadcbdd947e4128e3b88df8b233
Ajit Puthenputhussery <avp38@njit.edu>
2017-09-28 03:26:17 -0400

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -12,3 +12,12 @@ pq.add(10);
 pq.add(5);
 pq.peek();// To peek top most min element --> Will not remove element
 pq.poll();//To get top most min element --> Will remove element
+
+
+// If it is a user-definbed class --> Then lambda function cannot be used
+// So use comparator function
+PriorityQueue<Element> pq = new PriorityQueue<Element>(new Comparator<Element>() {
+			public int compare(Element a, Element b) {
+				return a.value - b.value;
+			}
+		});
