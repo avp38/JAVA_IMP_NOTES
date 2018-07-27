c43dcd9cdf474fd039115f74df22807f7628e7ba
Ajit P <avp38@njit.edu>
2018-07-27 02:04:16 -0400

    Update Integer_Long_Tricks.java

--- Integer_Long_Tricks.java
+++ Integer_Long_Tricks.java
@@ -11,3 +11,9 @@ Arrays.sort(a, 2, a.length); // To sort from index position 2 till end (Note: en
 // To get MAX Integer val
 int maxInt = Integer.MAX_VALUE;
 // Similarly can use for min value --> Also works for long 
+
+//V-IMP --> When multiplying something for long --> make sure it has some long component int it
+//ex: 
+long high = 10*k +1; //Now if k is int and is INTEGER.MAX_VALUE --> this will give wrong ans
+//So use 
+long high = 10L*k +1;
